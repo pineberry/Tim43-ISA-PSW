c8486d095c5ebb5920b7fd8060fe53f2d2631a91
Teodora Mandic <smejuljko@gmail.com>
2019-11-05 16:14:57 +0100

    Inicijalna Spring aplikacija

---
+++ ISCC/src/main/java/isapsw/tim43/ISCC/IsccApplication.java
@@ -0,0 +1,13 @@
+package isapsw.tim43.ISCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsccApplication.class, args);
+	}
+
+}
---
+++ ISCC/src/test/java/isapsw/tim43/ISCC/IsccApplicationTests.java
@@ -0,0 +1,13 @@
+package isapsw.tim43.ISCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
