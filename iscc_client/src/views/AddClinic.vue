0b56bd2511e2df3d27f1abe0211da34ed13d9604
petkalitis <dusan.petkovic02@gmail.com>
2019-11-15 02:02:35 +0100

    Inicijalna klijent aplikacija

---
+++ iscc_client/src/views/AddClinic.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <form class="col-6" v-on:submit.prevent="registerSubmit" accept-charset="UTF-8" method="post">
+                <div class="form-group">
+                    <label for="inputName">Name</label>
+                    <input type="text" class="form-control" id="inputName" placeholder="Enter clinic name" v-model="name">
+                </div>
+                <div class="form-group">
+                    <label for="inputAddress">Address</label>
+                    <input type="text" class="form-control" id="inputAddress" placeholder="Enter clinic adress" v-model="address">
+                </div>
+                <div class="form-group">
+                    <label for="inputDescription">Description</label>
+                    <textarea id="inputDescription" class="form-control" rows="3" v-model="description"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary mb-2">Register</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+
+    import axios from "axios";
+
+    export default {
+        name: "AddClinic",
+        data : function () {
+            return {
+                name : undefined,
+                address : undefined,
+                description : undefined,
+                averageRating : 0
+            }
+        },
+        methods : {
+            registerSubmit : function () {
+                var clinic = {
+                    "name" : this.name,
+                    "address" : this.address,
+                    "description" : this.description,
+                    "averageRating" : this.averageRating
+                }
+
+               axios.post('http://localhost:8080/center/admin/register/clinic', clinic)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
