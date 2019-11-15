6b058a817894f1bf6cd436f9f4d695ea844be265
petkalitis <dusan.petkovic02@gmail.com>
2019-11-15 02:05:02 +0100

    Frontend za dodavanje nove klinike

---
+++ iscc_client/src/main.ts
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
