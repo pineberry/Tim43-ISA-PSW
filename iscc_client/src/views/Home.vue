f6c2f77673d0bb85c28a36fbcbb39b8d9599c0de
Teodora Mandic <teodora.95mandic@gmail.com>
2019-11-19 15:13:20 +0100

    Izmene na register front

--- iscc_client/src/views/Home.vue
+++ iscc_client/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home">
+  	<Register/>
   </div>
 </template>
 
 <script>
+import Register from "@/components/Register.vue";
 
 export default {
   name: "home",
   components: {
+  	Register
   }
 };
 </script>
