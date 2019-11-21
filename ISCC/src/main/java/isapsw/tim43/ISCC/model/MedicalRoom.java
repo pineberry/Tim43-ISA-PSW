a4524b7ab64c24175aa06e60e258d3688368d0f0
petkalitis <dusan.petkovic02@gmail.com>
2019-11-21 18:16:00 +0100

    Anotiranje modela MedicalRoom radi implementiranja Lombok

--- ISCC/src/main/java/isapsw/tim43/ISCC/model/MedicalRoom.java
+++ ISCC/src/main/java/isapsw/tim43/ISCC/model/MedicalRoom.java
@@ -1,5 +1,7 @@
 package isapsw.tim43.ISCC.model;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class MedicalRoom {
 	
 	@Id
@@ -18,41 +21,7 @@ public class MedicalRoom {
 	
 	@Column(name = "roomNumber", nullable = false)
 	private long roomNumber;
-	
-//	private Map<Date, Boolean> availability;
-	
-	public long getId() {
-		return id;
-	}
-	
-	public void setId(long id) {
-		this.id = id;
-	}
-	
-	public String getRoomName() {
-		return roomName;
-	}
-	
-	public void setRoomName(String roomName) {
-		this.roomName = roomName;
-	}
-	
-	public long getRoomNumber() {
-		return roomNumber;
-	}
-	
-	public void setRoomNumber(long roomNumber) {
-		this.roomNumber = roomNumber;
-	}
-	
-/*	public Map<Date, Boolean> getAvailability() {
-		return availability;
-	}
-	
-	public void setAvailability(Map<Date, Boolean> availability) {
-		this.availability = availability;
-	}
-*/	
+
 	public MedicalRoom(long id, String roomName, long roomNumber) {
 		super();
 		this.id = id;
