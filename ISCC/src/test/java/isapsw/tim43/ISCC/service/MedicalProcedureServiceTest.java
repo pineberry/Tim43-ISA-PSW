08c22e744e9db6b27ae7d8662816bae8b3368971
petkalitis <dusan.petkovic02@gmail.com>
2020-02-09 16:10:19 +0100

    brisanje nepotrebnih importa

--- ISCC/src/test/java/isapsw/tim43/ISCC/service/MedicalProcedureServiceTest.java
+++ ISCC/src/test/java/isapsw/tim43/ISCC/service/MedicalProcedureServiceTest.java
@@ -1,7 +1,5 @@
 package isapsw.tim43.ISCC.service;
 
-import static isapsw.tim43.ISCC.constants.MedicalRoomConstants.ROOM_NAME;
-import static isapsw.tim43.ISCC.constants.MedicalRoomConstants.ROOM_NUMBER;
 import static isapsw.tim43.ISCC.constants.MedicalProcedureConstants.MEDICAL_PROCEDURE_BOOKED;
 import static isapsw.tim43.ISCC.constants.MedicalProcedureConstants.MEDICAL_PROCEDURE_CLINIC_RATED;
 import static isapsw.tim43.ISCC.constants.MedicalProcedureConstants.MEDICAL_PROCEDURE_DATE;
@@ -10,28 +8,19 @@ import static isapsw.tim43.ISCC.constants.MedicalProcedureConstants.MEDICAL_PROC
 import static isapsw.tim43.ISCC.constants.MedicalProcedureConstants.MEDICAL_PROCEDURE_DOCTOR_RATED;
 import static isapsw.tim43.ISCC.constants.MedicalProcedureConstants.MEDICAL_PROCEDURE_ID;
 import static isapsw.tim43.ISCC.constants.MedicalProcedureConstants.MEDICAL_PROCEDURE_PATIENT;
-import static isapsw.tim43.ISCC.constants.MedicalProcedureConstants.MEDICAL_PROCEDURE_ROOM;
 import static isapsw.tim43.ISCC.constants.MedicalProcedureConstants.MEDICAL_PROCEDURE_TYPE;
 import static isapsw.tim43.ISCC.constants.MedicalRoomConstants.ROOM_CLINIC;
-
+import static isapsw.tim43.ISCC.constants.MedicalRoomConstants.ROOM_NAME;
+import static isapsw.tim43.ISCC.constants.MedicalRoomConstants.ROOM_NUMBER;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
